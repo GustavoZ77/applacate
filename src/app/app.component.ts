b66bf00d948bd95da6876e3a2d81d9cf0dea2561
GustavoZ77 <kyg7887@gmail.com>
2017-01-29 11:22:42 -0600

    adding colors theme and layout structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Auth } from './services/security/auth.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule  }      from '@angular/platform-browser';
 import { HttpModule, Http, RequestOptions } from '@angular/http';
 import { Auth } from './services/security/auth.service';
 import { AppComponent }        from './app.component';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 
 @NgModule({
@@ -14,6 +16,7 @@ import { AppComponent }        from './app.component';
   ],
   imports: [
     BrowserModule,
+    MaterialModule.forRoot()
   ],
   bootstrap: [AppComponent]
 })
